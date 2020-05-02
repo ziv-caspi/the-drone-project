862c17e9852617692f8fb2289705f3b784bc8601
zivCaspi <caspifx@gmail.com>
2020-05-03 00:49:04 +0300

    Simple UDP Server Test

--- pi/udp_server.py
+++ pi/udp_server.py
@@ -2,4 +2,12 @@ import socket
 import random
 import hashlib
 
-print('Bringing UDP Server UP.')
+
+def main():
+    server_socket = socket.socke(socket.AF_INET, socket.SOCK_DGRAM)
+    server_socket.bind(('127.0.0.1', 7777))
+
+    while True:
+        msg, addrs = server_socket.recvfrom(1024)
+        msg = msg.decode()
+        print(msg, 'From:', addrs)
