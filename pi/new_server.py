8aa9efa9d2dbf995c070c9bb2d9c59c1095cb8ec
zivCaspi <caspifx@gmail.com>
2020-05-13 18:30:43 +0300

    Reps Limit Fix

--- pi/new_server.py
+++ pi/new_server.py
@@ -22,6 +22,7 @@ class Server():
         self.client_connected = False
 
         self.PASSWORD = PASSWORD
+        self.SEED = SALT_SEED
         random.seed(SALT_SEED)
         self.RANDOM_LIMIT = 99999999
         self.REPS_LIMIT = 10
@@ -88,6 +89,8 @@ class Server():
         self.server_socket.listen(self.CONNECTIONS)
 
     def gen_new_session_salt(self):
+        if self.randoms_used == self.RANDOM_LIMIT:
+            random.seed(self.SEED ** 2)
         self.session_salt = random.randint(0, self.RANDOM_LIMIT)
         self.randoms_used += 1
         self.current_salt = self.session_salt
