1c9982387dd8a10f00a3759d2a4154124a52030c
zivCaspi <caspifx@gmail.com>
2020-05-15 15:52:00 +0100

    Added Beep Audio File And Script that takes binary number and play sound every 0.1 seconds if is 1 and nothing if 0

---
+++ pi/bin_to_sound.py
@@ -0,0 +1,24 @@
+import time
+import sys
+import simpleaudio as sa
+
+def play_sound(filename, dur):
+    wave = sa.WaveObject.from_wave_file(filename)
+    play = wave.play()
+    time.sleep(dur)
+    play.stop()
+
+def bin_to_sound(bin_string):
+    for val in bin_string:
+        if val == '1':
+            play_sound('beep-01a.wav', 0.1)
+        else:
+            pass
+
+
+
+while True:
+    bin_string = input('Enter Bin String: ')
+    bin_to_sound(bin_string)
+    
+
