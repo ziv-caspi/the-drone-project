12ae1dc1309a49a6ac979c46d5cad4f3bee89790
zivCaspi <caspifx@gmail.com>
2020-05-15 13:19:41 +0300

    Reps Limit Fix

--- pi/new_server.py
+++ pi/new_server.py
@@ -24,7 +24,7 @@ class Server():
         self.PASSWORD = PASSWORD
         self.SEED = SALT_SEED
         random.seed(SALT_SEED)
-        self.RANDOM_LIMIT = 99999999
+        self.RANDOM_LIMIT = 10
         self.REPS_LIMIT = 10
         self.reps_file_path = 'random_reps.txt'
         try:
