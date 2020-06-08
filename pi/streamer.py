e2cb2cb56924a71b84431f9f0f61221277b576ad
zivCaspi <caspifx@gmail.com>
2020-06-08 18:32:13 +0300

    Fix to Streamer make it udp

--- pi/streamer.py
+++ pi/streamer.py
@@ -9,36 +9,31 @@ class Streamer():
     def __init__(self, timeout):
         self.timeout = timeout
         self.client_addrs = None
-        self.client_socket = None
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.key = None
 
     def start_streaming_to_client(self, client_addrs):
         self.client_addrs = client_addrs
-        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.client_socket.settimeout(self.timeout)
         try:
-            self.client_socket.connect(client_addrs)
             self.vs = VideoStream(src=0).start()
             self.streaming_loop()
 
         except socket.timeout:
             print('Socket Time Out Exceeded, Closing Streaming Connection')
-            self.client_socket.close()
             self.client_addrs = None
 
         except socket.error:
             print('Lost Streaming Connection.')
-            self.client_socket.close()
             self.client_addrs = None
 
     def stop_streaming_to_client(self):
-        self.client_socket.close()
         self.client_addrs = None
 
     def streaming_loop(self):
-        while True:
+        while self.client_addrs:
             frame = self.vs.read()
             frame = resize(frame, width=320)
             frame = frame.tobytes()
-            self.client_socket.send(str(len(frame)).encode())
-            self.client_socket.send(frame)
+            self.client_socket.sendto(str(len(frame)).encode(), self.client_addrs)
+            self.client_socket.sendto(frame, self.client_addrs)
